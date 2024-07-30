01ca1c1a97da1bc18cd15f103e8a8c2ac9fc9137
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-30 16:49:12 -0300

    finish basic hover counting characters

--- src/analysis/state.go
+++ src/analysis/state.go
@@ -1,5 +1,11 @@
 package analysis
 
+import (
+	"fmt"
+
+	"github.com/brunobmello25/educationalsp/src/lsp"
+)
+
 type State struct {
 	Documents map[string]string
 }
@@ -16,4 +22,20 @@ func (s *State) OpenDocument(uri string, text string) {
 
 func (s *State) UpdateDocument(uri string, text string) {
 	s.Documents[uri] = text
+}
+
+func (s *State) Hover(id int, uri string, position lsp.Position) lsp.HoverResponse {
+	document := s.Documents[uri]
+
+	response := lsp.HoverResponse{
+		Response: lsp.Response{
+			RPC: "2.0",
+			ID:  &id,
+		},
+		Result: lsp.HoverResult{
+			Contents: fmt.Sprintf("File: %s, characters: %d", uri, len(document)),
+		},
+	}
+
+	return response
 }
